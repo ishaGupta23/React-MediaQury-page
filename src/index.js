35231f00c7973e8f8a5caa83ff4d9078104b0504
ishaGupta23 <ishagupta77@gmail.com>
2023-03-21 11:12:51 +0530

    we create a page nd set page media query

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+import Problem from './components/Prob';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    
+    <Problem/>
+    
   </React.StrictMode>
 );
 
